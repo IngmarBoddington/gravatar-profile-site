c507fb1cc04e63764fe0136d36f5c145eccb4e92
Ingmar Boddington <ingmarboddington@gmail.com>
2014-09-24 20:40:02 +0000

    Moved tracking code into head, made optional through var

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ $services = array(
 $profileImage = 'image/profile.jpeg';
 $backgroundImage = 'image/background.jpg';
 $text = 'Codemonkey / metalhead living in Sheffield, UK. Currently working at <a href="http://www.plus.net/">plus.net</a> on their php based sites and backend systems, has a (dormant) codey blog at <a href="http://www.glowingminds.co.uk/">glowingminds.co.uk</a>. Used to work at <a href="http://www.bunches.co.uk/">Bunches.co.uk</a>, Nottingham Uni Graduate, Zend Certified Engineer.';
+$trackingCode = 'UA-7152965-11';
 
 ?>
 <!DOCTYPE html>
@@ -26,6 +27,18 @@ $text = 'Codemonkey / metalhead living in Sheffield, UK. Currently working at <a
 	    <title><?php echo $name ? $name : ''; ?></title>
 	    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" user-scalable="no" >
 	    <link rel="stylesheet" type="text/css" href="style.css" />
+        <?php if ($trackingCode): ?>
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', '<?php echo $trackingCode; ?>', 'auto');
+            ga('send', 'pageview');
+
+        </script>
+        <?php endif; ?>
     </head>
 
     <body<?php $backgroundImage ? '' : ' class="no-background"'; ?>>
@@ -33,40 +46,30 @@ $text = 'Codemonkey / metalhead living in Sheffield, UK. Currently working at <a
         <div id="container" class="h-card vcard">
 
             <?php if ($backgroundImage) : ?>
-                <div id="cover" style="background-image:url( <?php echo $backgroundImage; ?> );"></div>
+            <div id="cover" style="background-image:url( <?php echo $backgroundImage; ?> );"></div>
             <?php endif; ?>
 
             <div id="profile">
 
                 <div id="bio" class="section">
                     <?php if ($profileImage) : ?>
-                        <img class="u-photo photo" height="80" width="80" src="<?php echo $profileImage; ?>?s=160" />
+                    <img class="u-photo photo" height="80" width="80" src="<?php echo $profileImage; ?>?s=160" />
                     <?php endif; ?>
                     <h1 class="p-name fn"><?php echo $name; ?></h1>
                     <?php if (isset($text)) : ?>
-                         <p class="p-note"><?php echo $text; ?></p>
+                    <p class="p-note"><?php echo $text; ?></p>
                     <?php endif; ?>
                 </div>
 
                 <?php if ( !empty( $services ) ) : ?>
-                    <ul id="accounts" class="section">
-                        <?php foreach ( $services as $service => $url ) : ?>
-                            <li class="<?php echo strtolower(str_replace('.', '', $service)); ?>"><a class="u-url url" rel="me" href="<?php echo $url; ?>"><?php echo $service; ?></a></li>
-                        <?php endforeach; ?>
-                    </ul>
+                <ul id="accounts" class="section">
+                    <?php foreach ( $services as $service => $url ) : ?>
+                    <li class="<?php echo strtolower(str_replace('.', '', $service)); ?>"><a class="u-url url" rel="me" href="<?php echo $url; ?>"><?php echo $service; ?></a></li>
+                    <?php endforeach; ?>
+                </ul>
                 <?php endif; ?>
 
             </div>
         </div>
-        <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-            ga('create', 'UA-7152965-11', 'auto');
-            ga('send', 'pageview');
-
-        </script>
     </body>
 </html>
